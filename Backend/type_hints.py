177257ec8bc4f86bbcfa6ffbb2ff207e0ac34ca8
pablo.echegaray <pablo.echegaray97@gmail.com>
2023-02-26 13:27:53 -0300

    first commit

---
+++ Backend/type_hints.py
@@ -0,0 +1,15 @@
+### type hints ##
+my_string_variable = "My String variable"
+print (my_string_variable)
+print (type(my_string_variable))
+
+my_string_variable = 5
+print (type(my_string_variable))
+
+my_typed_variable : str = "My typed String variable"
+
+print (type(my_typed_variable))
+my_typed_variable = 5
+print (type(my_typed_variable))
+
+## podemos decirle que tipo es pero es un tipado debil, no podemos obligar a la variable a ser siempre un string si despues cambia ##
